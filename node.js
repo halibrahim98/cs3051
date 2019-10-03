ddac15ac59daa6293426c45b5d971324161c209f
halibrahim98 <hussain.alibrahim.m@gmail.com>
2019-10-03 14:41:11 +0000

    did countdown and isEven / forEach

--- node.js
+++ node.js
@@ -1,8 +1,40 @@
-let sum = function(array) {
-    let counter = 0
-    for (let i=0; i < array.length; i++) {
-        counter += array[i]
+//isEven predicate + filter
+
+let isEven = function(n) {
+    if(n%2) {
+        return true
     }
-    return counter
+    else {
+        return false
+    }
 }
-console.log(sum[1, 2, 3, 4, 5])
+let i = 0
+let even = []
+let forEach = function(array, pred) {
+    if (pred(array[i]) != true && i < array.length) {
+        array.splice(i, 1)
+        forEach(array[i+1])
+    }
+
+}
+
+let lol = [1, 2, 3, 4, 5]
+forEach(lol, isEven())
+let printarr = function(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+printarr(lol)
+
+//Countdown
+
+let timer = function(num) {
+    setTimeout(function() {
+        if (num >= 0) {
+            console.log(num)
+            timer(num-1)
+        }
+    }, 1000)
+}
+timer(10)
